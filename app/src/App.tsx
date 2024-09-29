b8e334e7797b413bf273e3ee7e02f5e747ab0e63
JonnaMat <matthiesenjonna@gmail.com>
2024-09-29 19:09:12 +0200

    Add LandingPage

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,17 @@
 import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 
 function App() {
   return (
-    <div className={styles.App}>
-      Arch
-      <ThemeSwitcher />
-    </div>
+    <Router basename="/arch">
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ app/src/components/LandingPage.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ThemeSwitcher from "./ThemeSwitcher";
+import styles from "./LandingPage.module.css";
+
+const LandingPage = () => {
+  const colors = [
+    "var(--purple)",
+    "var(--blue)",
+    "var(--pink)",
+    "var(--orange)",
+    "var(--green)",
+    "var(--yellow)",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * colors.length);
+
+  const randomColor = colors[randomIndex];
+
+  console.log("color", randomColor);
+
+  return (
+    <div className={styles.LandingPage}>
+      <ThemeSwitcher />
+      <h1 className={`${styles.MovingGradientText} ${styles.Title}`}>Arch</h1>
+      <h2 className={`${styles.MovingGradientText} ${styles.Subtitle}`}>
+        Lorem ipsum dolor sit amet!
+      </h2>
+      <Link to="/blog" className={styles.Text} style={{ color: randomColor }}>
+        Visit Blog
+      </Link>
+    </div>
+  );
+};
+
+export default LandingPage;

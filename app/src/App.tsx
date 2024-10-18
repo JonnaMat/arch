bd25723c4166db21ef058a6b256d966e4df84ed3
JonnaMat <matthiesenjonna@gmail.com>
2024-10-18 22:26:13 +0200

    Minimal App for testing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,24 +2,40 @@ import styles from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import ThemeSwitcher from "./components/ThemeSwitcher";
-import Card from "./components/Card";
-import CardGrid from "./components/CardGrid";
+import Deck from "./components/Deck";
 
 function App() {
+  const cards = [
+    { title: "Card 1", content: "This is card 1 content." },
+    { title: "Card 2", content: "This is card 2 content." },
+    { title: "Card 3", content: "This is card 3 content." },
+    { title: "Card 4", content: "This is card 4 content." },
+    { title: "Card 5", content: "This is card 5 content." },
+  ];
   return (
     <Router basename="/arch">
       <div>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
           <Route
-            path="/card"
+            path="/"
             element={
-              <div style={{ width: "300px" }}>
-                <Card title="Title" content="hello" />
+              <div>
+                {" "}
+                <ThemeSwitcher />
+                <LandingPage />
               </div>
             }
           />
-          <Route path="/grid" element={<CardGrid />} />
+          <Route
+            path="/deck"
+            element={
+              <div>
+                {" "}
+                <ThemeSwitcher />
+                <Deck cards={cards} />
+              </div>
+            }
+          />
         </Routes>
       </div>
     </Router>
